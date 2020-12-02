e3b3eac12dc9056550107e5b1eab55f5f20d71b1
Jao42 <joaovictormedeiros41@gmail.com>
2020-12-02 15:35:23 -0300

    video version

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,16 +1,14 @@
-keys = document.querySelectorAll('.key');
-sounds = document.querySelectorAll('audio');
+window.addEventListener('keydown', (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+  key.addEventListener('transitionend', () => {key.classList.remove('playing')})
 
-keys.forEach((key) => {
-  window.addEventListener('keydown', (e) => {
-  if (e.keyCode == key.getAttribute('data-key')) {
-    sounds.forEach((sound) => {
-    if (sound.getAttribute('data-key') == e.keyCode) sound.play();
-    
-    
-    
-    })
-    }
-  })
 })
+
+
+  
 
